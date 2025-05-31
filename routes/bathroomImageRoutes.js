9afe13e787ce503e50f5b520aec1a59b172fbbcb
ascoggins13 <aaron.scoggins13@gmail.com>
2025-05-31 02:59:38 -0500

    updated bathroomImageRoutes

--- routes/bathroomImageRoutes.js
+++ routes/bathroomImageRoutes.js
@@ -1,11 +1,10 @@
 const express = require('express');
-const { addBathroomImage } = require('../controllers/locations.controller');
 const multer = require('multer');
-const upload = multer(); // For handling multipart/form-data
+const { addBathroomImage } = require('../controllers/locations.controller');
 
+const upload = multer();
 const router = express.Router();
 
-// Correct route: will map to /api/bathrooms/:id/images
 router.post('/:id/images', upload.single('image'), addBathroomImage);
 
 module.exports = router;
